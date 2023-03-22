4fc002e061630351d5785bfd04e4b40a07f08548
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2023-03-22 23:44:13 +0300

    tile added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Netflix</title>
+        <meta
+          name='description'
+          content={'Neftlix Clone'}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
