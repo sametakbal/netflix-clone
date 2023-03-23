ac42de288c6ab1df3a738aa17c67039678765212
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2023-03-23 23:15:21 +0300

    Fix #2: Profiles screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,37 @@
-import { Inter } from 'next/font/google';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
 
-const inter = Inter({ subsets: ['latin'] });
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <div>
-        <h1>Netflix Clone</h1>
+        <h1 className='text-white'>Netflix Clone</h1>
+        <p className='text-white'>Logged as : {user?.name}</p>
+        <button
+          className='h-10 w-full bg-white'
+          onClick={() => signOut()}
+        >
+          Sign Out
+        </button>
       </div>{' '}
     </>
   );
