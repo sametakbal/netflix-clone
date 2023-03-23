87a9775a34aecd2fa6b394f9b077bc5fb7fb6cbb
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2023-03-24 01:22:53 +0300

    Fix #5: login defects

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,6 +1,7 @@
 import useCurrentUser from '@/hooks/useCurrentUser';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -19,6 +20,7 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profiles = () => {
+  const router = useRouter();
   const { data: user } = useCurrentUser();
 
   return (
@@ -28,7 +30,7 @@ const Profiles = () => {
           Who is watching
         </h1>
         <div className='flex items-center justify-center gap-8 mt-10'>
-          <div onClick={() => {}}>
+          <div onClick={() => router.push('/')}>
             <div
               className='
                     w-44
