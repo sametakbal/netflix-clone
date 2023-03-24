9639fec59e0af04f39face4a87f9600b1e7a6985
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2023-03-24 23:24:55 +0300

    navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import { NextPageContext } from 'next';
 import { getSession, signOut } from 'next-auth/react';
@@ -23,16 +24,7 @@ export default function Home() {
 
   return (
     <>
-      <div>
-        <h1 className='text-white'>Netflix Clone</h1>
-        <p className='text-white'>Logged as : {user?.name}</p>
-        <button
-          className='h-10 w-full bg-white'
-          onClick={() => signOut()}
-        >
-          Sign Out
-        </button>
-      </div>{' '}
+      <Navbar />
     </>
   );
 }
