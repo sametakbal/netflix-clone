38f9155953e3ae6299e944f92975bff9c7c167b6
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2023-03-26 18:49:49 +0300

    Fix #6: movie list

---
+++ pages/api/movies/index.ts
@@ -0,0 +1,23 @@
+import serverAuth from '@/libs/serverAuth';
+import prismadb from '@/libs/prismadb';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    if (req.method !== 'GET') {
+      return res.status(405).end();
+    }
+
+    await serverAuth(req);
+
+    const movies = await prismadb.movie.findMany();
+
+    return res.status(200).json(movies);
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).end();
+  }
+}
