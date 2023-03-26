45b8e4af07f7ae1d42e2af212f6863a7979b2526
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2023-03-26 17:07:50 +0300

    Fix #4: billboard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -34,19 +34,9 @@ const Navbar = () => {
   return (
     <nav className='w-full fixed z-40'>
       <div
-        className={`
-          px-4
-        md:px-16
-        py-6
-        flex
-        flex-row
-        items-center
-        transition
-        duration-500
-        bg-zinc-900
-        bg-opacity-90
-        ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}
-          `}
+        className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${
+          showBackground ? 'bg-zinc-900 bg-opacity-90' : ''
+        }`}
       >
         <img
           className='h-4 lg:h-7'
